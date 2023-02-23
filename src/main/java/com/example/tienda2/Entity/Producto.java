184c2c0019d83e685aee4443ac41e3e05d987c52
DavidQ2001 <61300628+DavidQ2001@users.noreply.github.com>
2023-02-23 15:07:11 -0500

    proyectocrud

---
+++ src/main/java/com/example/tienda2/Entity/Producto.java
@@ -0,0 +1,54 @@
+package com.example.tienda2.Entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "productos")
+public class Producto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+
+    private Double precio;
+    @Column(name = "fecha_creacion")
+    private Date fechaCreacion;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+}
